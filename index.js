0c38ef9f074dce0c12831df1f6e2a15dee5e2f3a
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-26 10:45:01 +0300

    creates wrap adjective

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun (activity ='roller-skate'){
+  return `This Saturday, I want to ${activity}!`
+  }
+//console.log(saturdayFun("bathe my dog!"))
+
+function mondayWork (to_do='go to the office'){
+  return `This Monday, I will ${to_do}.`
+}
+//console.log(mondayWork())
+
+function wrapAdjective (flair = '*'){
+  return function (tam ='special'){
+    return `You are ${flair}${tam}${flair}!`
+  }
+}
+wrapAdjective()('*')
+//console.log(wrapAdjective())
