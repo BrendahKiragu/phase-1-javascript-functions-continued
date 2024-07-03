9f02b232416ed3bb6621672e89ef719be88658cb
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-03 23:31:06 +0300

    works on js functions

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function saturdayFun (activity ='roller-skate'){
   return `This Saturday, I want to ${activity}!`
   }
+//console.log(saturdayFun())
 //console.log(saturdayFun("bathe my dog!"))
 
 function mondayWork (to_do='go to the office'){
   return `This Monday, I will ${to_do}.`
 }
-//console.log(mondayWork())
+//console.log(mondayWork('go swimming'))
 
 function wrapAdjective (flair = '*'){
   return function (adjective ='special'){
     return `You are ${flair}${adjective}${flair}!`
   }
 }
-//wrapAdjective()('*')
-//console.log(wrapAdjective())
+// you can change the flair argument as well as the adjective
+//wrapAdjective('#')('a dedicated programmer')
+//console.log(wrapAdjective('#')('a dedicated programmer'))
